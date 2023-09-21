03241982e9a6bb2b0e5395dae75c741261cc1962
Shobhit Gupta <guptashobhit18@gmail.com>
2023-09-21 15:44:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon");
 streamlit.text(fruityvice_response.json());
+
+# Normalize json version of fruit advice
+fruityvice_response_normalized = pandas.json_normalize(fruityvice_response.json());
+# output it as table
+streamlit.dataframe(fruityvice_response_normalized)
